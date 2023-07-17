8163a36bc4cca10539af0bb42b799784fbdb02b3
nlscott83 <113377073+nlscott83@users.noreply.github.com>
2023-07-17 09:53:00 -0500

    First commit

---
+++ src/main/java/com/nlscott83/jenkinssampleapp/JenkinsSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.nlscott83.jenkinssampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nlscott83/jenkinssampleapp/JenkinsSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nlscott83.jenkinssampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
